0a1f0390cfbf491e34674d12af3da5544d0db923
Thomas Montague <montague.thomas@lmbbox.com>
2025-11-26 01:05:57 -0500

    Adjust some whitespacing.

--- src/simarine/protocol.py
+++ src/simarine/protocol.py
@@ -28,13 +28,13 @@ class MessageType(IntEnum):
   Request system information.
 
   Request:
-      0000000000 ff 01 00000000 0003 ff 89b8
+    0000000000 ff 01 00000000 0003 ff 89b8
 
   Response:
-      0000000000 ff 01 84b3ee93 0011
-      ff 01 01 84b3ee93   -> Serial Number (uint32: 84B3EE93)
-      ff 02 01 00010015   -> Firmware Version (int16.int16: 0001.0015)
-      ff 97a3
+    0000000000 ff 01 84b3ee93 0011
+    ff 01 01 84b3ee93   -> Serial Number (uint32: 84B3EE93)
+    ff 02 01 00010015   -> Firmware Version (int16.int16: 0001.0015)
+    ff 97a3
   """
 
   DEVICE_SENSOR_COUNT = 0x02
@@ -42,13 +42,13 @@ class MessageType(IntEnum):
   Request device and sensor counts.
 
   Request:
-      0000000000 ff 02 00000000 0003 ff 7688
+    0000000000 ff 02 00000000 0003 ff 7688
 
   Response:
-      0000000000 ff 02 84b3ee93 0011
-      ff 01 01 00000013   -> Device Count / Last Device Id (zero index)
-      ff 02 01 0000001a   -> Sensor Count / Last Sensor Id (zero index)
-      ff 76e9
+    0000000000 ff 02 84b3ee93 0011
+    ff 01 01 00000013   -> Device Count / Last Device Id (zero index)
+    ff 02 01 0000001a   -> Sensor Count / Last Sensor Id (zero index)
+    ff 76e9
   """
 
   DEVICE_INFO = 0x41
@@ -56,22 +56,22 @@ class MessageType(IntEnum):
   Request device information for a specific device index.
 
   Request:
-      0000000000 ff 41 00000000 0016
-      ff 00 01 0000000b               -> Device Id
-      ff 01 03 00000000 ff 00000000   -> Unknown
-      ff fe6d
+    0000000000 ff 41 00000000 0016
+    ff 00 01 0000000b               -> Device Id
+    ff 01 03 00000000 ff 00000000   -> Unknown
+    ff fe6d
 
   Response:
-      0000000000 ff 41 84b3ee93 005a
-      ff 00 01 0000000b
-      ff 01 03 65932547 ff 00000001
-      ff 02 01 00000000
-      ff 03 04 65932547ff5343353033205b313736355d2031 00
-      ff 04 01 2cb15f45
-      ff 05 01 00000001
-      ff 06 03 678ef359 ff 00000011
-      ff 07 03 00000000 ff 39420bbb
-      ff 4e7f
+    0000000000 ff 41 84b3ee93 005a
+    ff 00 01 0000000b
+    ff 01 03 65932547 ff 00000001
+    ff 02 01 00000000
+    ff 03 04 65932547ff5343353033205b313736355d2031 00
+    ff 04 01 2cb15f45
+    ff 05 01 00000001
+    ff 06 03 678ef359 ff 00000011
+    ff 07 03 00000000 ff 39420bbb
+    ff 4e7f
 
   Response contains structured binary fields:
   - Field 0: Device Id
@@ -84,27 +84,27 @@ class MessageType(IntEnum):
   Request sensor information for given device index.
 
   Request:
-      0000000000 ff 20 00000000 0011
-      ff 01 01 00000002   -> Sensor Id
-      ff 02 01 00000000   -> Unknown
-      ff 74ee
+    0000000000 ff 20 00000000 0011
+    ff 01 01 00000002   -> Sensor Id
+    ff 02 01 00000000   -> Unknown
+    ff 74ee
 
   Response:
-      0000000000 ff 20 84b3ee93 008b
-      ff 01 01 00000002
-      ff 02 01 00000005
-      ff 03 01 00000005
-      ff 04 01 00000000
-      ff 05 03 00000000 ff 00000000
-      ff 06 03 00000000 ff 32ff1202
-      ff 07 03 00000000 ff 012c0014
-      ff 08 03 00000000 ff 00000000
-      ff 09 03 00000000 ff 32ff1202
-      ff 0a 03 00000000 ff 012c0014
-      ff 0b 03 00000000 ff 00000002
-      ff 0c 03 00000000 ff 00000000
-      ff 0d 03 00000000 ff 00000000
-      ff 6fd9
+    0000000000 ff 20 84b3ee93 008b
+    ff 01 01 00000002
+    ff 02 01 00000005
+    ff 03 01 00000005
+    ff 04 01 00000000
+    ff 05 03 00000000 ff 00000000
+    ff 06 03 00000000 ff 32ff1202
+    ff 07 03 00000000 ff 012c0014
+    ff 08 03 00000000 ff 00000000
+    ff 09 03 00000000 ff 32ff1202
+    ff 0a 03 00000000 ff 012c0014
+    ff 0b 03 00000000 ff 00000002
+    ff 0c 03 00000000 ff 00000000
+    ff 0d 03 00000000 ff 00000000
+    ff 6fd9
 
   Response provides:
   - Sensor Id
@@ -117,35 +117,35 @@ class MessageType(IntEnum):
   Request sensors state.
 
   Request:
-      0000000000 ff b0000000000003 ff e237
+    0000000000 ff b0000000000003 ff e237
 
   Response:
-      0000000000 ff b0 84b3ee93 00ab
-      ff 00 01 691c8a3c
-      ff 01 01 691c43ea
-      ff 02 01 fffffc16
-      ff 03 01 00018e8f
-      ff 04 01 fffffc16
-      ff 05 01 0000342a
-      ff 09 01 691c8947
-      ff 0a 01 fffffc16
-      ff 0b 01 000001dd
-      ff 0c 01 0008d88b
-      ff 0d 01 000033f4
-      ff 0e 01 00000000
-      ff 0f 01 0000ffff
-      ff 10 01 0000ffff
-      ff 11 01 00005e64
-      ff 12 01 00000039
-      ff 13 01 33410062
-      ff 14 01 000001dd
-      ff 15 01 000033f4
-      ff 16 01 7fffffff
-      ff 17 01 000133e4
-      ff 18 01 ffffffc9
-      ff 19 01 00000035
-      ff 1a 01 7fffffff
-      ff 1ec5
+    0000000000 ff b0 84b3ee93 00ab
+    ff 00 01 691c8a3c
+    ff 01 01 691c43ea
+    ff 02 01 fffffc16
+    ff 03 01 00018e8f
+    ff 04 01 fffffc16
+    ff 05 01 0000342a
+    ff 09 01 691c8947
+    ff 0a 01 fffffc16
+    ff 0b 01 000001dd
+    ff 0c 01 0008d88b
+    ff 0d 01 000033f4
+    ff 0e 01 00000000
+    ff 0f 01 0000ffff
+    ff 10 01 0000ffff
+    ff 11 01 00005e64
+    ff 12 01 00000039
+    ff 13 01 33410062
+    ff 14 01 000001dd
+    ff 15 01 000033f4
+    ff 16 01 7fffffff
+    ff 17 01 000133e4
+    ff 18 01 ffffffc9
+    ff 19 01 00000035
+    ff 1a 01 7fffffff
+    ff 1ec5
 
   Response provides:
   - Sensor Id
@@ -157,14 +157,14 @@ class MessageType(IntEnum):
   Unknown type 0x03
 
   Request:
-      0000000000 ff 03 00000000 000a
-      ff 01 01 00000000
-      ff 24d2
+    0000000000 ff 03 00000000 000a
+    ff 01 01 00000000
+    ff 24d2
 
   Response:
-      0000000000 ff 03 84b3ee93 000a
-      ff 01 01 00000001
-      ff b56e
+    0000000000 ff 03 84b3ee93 000a
+    ff 01 01 00000001
+    ff b56e
   """
 
   UNKNOWN_10 = 0x10
@@ -172,15 +172,15 @@ class MessageType(IntEnum):
   Unknown type 0x10
 
   Request:
-      0000000000 ff 10 00000000 0011
-      ff 01 01 691c8a3a   -> timestamp
-      ff 02 01 ffffb9b0   -> int32? -18000
-      ff 0301
+    0000000000 ff 10 00000000 0011
+    ff 01 01 691c8a3a   -> timestamp
+    ff 02 01 ffffb9b0   -> int32? -18000
+    ff 0301
 
   Response:
-      0000000000 ff aa 84b3ee93 000a
-      ff 01 01 0000ff10
-      ff 85d5
+    0000000000 ff aa 84b3ee93 000a
+    ff 01 01 0000ff10
+    ff 85d5
   """
 
   UNKNOWN_50 = 0x50
@@ -188,12 +188,12 @@ class MessageType(IntEnum):
   Unknown type 0x50
 
   Request:
-      0000000000 ff 50 00000000 0003 ff 3036
+    0000000000 ff 50 00000000 0003 ff 3036
 
   Response:
-      0000000000 ff aa 84b3ee93 000a
-      ff 01 01 0000ff50   -> int32? 65360
-      ff a1b1
+    0000000000 ff aa 84b3ee93 000a
+    ff 01 01 0000ff50   -> int32? 65360
+    ff a1b1
   """
 
   UNKNOWN_AA = 0xAA
@@ -201,15 +201,15 @@ class MessageType(IntEnum):
   Unknown type 0xAA
 
   Request:
-      0000000000 ff 10 00000000 0011
-      ff 01 01 691c8a3a
-      ff 02 01 ffffb9b0
-      ff 0301
+    0000000000 ff 10 00000000 0011
+    ff 01 01 691c8a3a
+    ff 02 01 ffffb9b0
+    ff 0301
 
   Response:
-      0000000000 ff aa 84b3ee93 000a
-      ff 01 01 0000ff10
-      ff 85d5
+    0000000000 ff aa 84b3ee93 000a
+    ff 01 01 0000ff10
+    ff 85d5
   """
 
   UNKNOWN_C1 = 0xC1
@@ -226,9 +226,56 @@ class MessageType(IntEnum):
   - Likely internal sampling buffer or oscilloscope view
   - Probably intended only for diagnostics or internal UI
 
-  Response: 0000000000 ff c1 84b3ee93 0477 ff 00 0b 691c89f0 ff 691c89f0 ff e1 ff 560b560a ff 560f5611 ff 560e5609ff560b560fff5611561aff56195621ff5624562aff561e5612ff5619561fff5614560dff5611560cff5606560eff560b5616ff5613560bff560555fdff55f155eaff55db55d4ff55d155d4ff55ce55d2ff55b955a9ff55a05591ff558a5581ff556a556cff5567556eff55695563ff555d5549ff55315525ff551e5516ff5512550cff55075504ff550054fbff54f354f3ff54e854e5ff54df54ceff54be54b1ff54a854a6ff54a0549eff5499549fff549a5491ff54895489ff54865489ff54875477ff546c5462ff54545449ff5443543cff54335426ff541d5416ff5418540fff540453f6ff53e853ddff53ce53c6ff53ba53b3ff53a85399ff53805371ff5362535cff5355534aff533e5333ff5322530fff52f952ddff52d752c5ff52b952aaff52985283ff52695258ff524d523dff522c5221ff520c51f9ff51ec51e0ff51c651b1ff51a651a4ff51a451a6ff51b251adff51aa51b8ff51b351b8ff51bd51b7ff51b551c7ff51bd51c2ff51b151b6ff51af51b2ff51b051b8ff51b751afff51a05194ff518f5189ff518d5186ff5185517cff51745165ff5154514aff5134512bff511c5109ff50ed50dcff50d150bcff50a95095ff50775060ff50465036ff50215015ff5010500dff50015000ff4ff54fe7ff4fdc4fc6ff4fae4fa1ff4f984f89ff4f814f85ff4f804f74ff4f6d4f6aff4f684f63ff4f5d4f57ff4f534f4aff4f444f42ff4f3b4f34ff4f314f31ff4f284f20ff4f204f1aff4f1a4f1bff4f1a4f17ff4f0a4efeff4efd4efcff4ef94ef1ff4eee4eefff4eed4ee7ff4ee74eecff4ee84ee9ff4ee44edfff4ece4ecdff4ebf4eb3ff4ea74e9bff4e9d4e93ff4e874e78ff4e694e51ff4e3d4e30ff4e184dffff4de34dc0ff4da44d8aff4d6a4d60ff4d504d37ff4d1d4d03ff4ceb4cddff4cc54cb4ff4ca24c93ff4c7f4c67ff4c654c46ff4c2d4c17ff4bff4bd9ff4bce4bd4ff4bcf4bbbff4bc94bbeff4be54be9ff4be54be6ff4bd54bceff4bc64bb0ff4ba74bb2ff4bb54bbeff4bbd4bc4ff4bbc4bb4ff4baa4b8cff4b654b72ff4b644b66ff4b574b5aff4b4e4b31ff4b164b00ff4afd4ae4ff4ac74ab1ff4a9d4a84ff4a7f4a69ff4a5a4a46ff4a404a37ff4a2d4a2aff4a244a18ff4a0f49fcff49f049f6ff49f649f3ff49f549ebff49dd49c0ff499f4994ff498d4976ff4971496aff4979499cff49a349a6ff49914999ff4998499aff49a349a2ff49bb49d1ff49e249f1ff49f549e8ff49fc4a03ff4a0b4a23ff4a354a59ff4a624a7cff4a894aa0ff4abb4ad8ff4ae74aecff4b024b1fff4b504b70ff4b994babff4bb24bb0ff4bd24bdfff4be94c0eff4c264c29ff4c3d4c62ff4c6e4c7bff4c904cb9ff4ce24cf7ff4d084d13ff4d184d06ff4d1e4d36ff4d3c4d3dff4d4d4d68ff4d914db0ff4dcd4ddfff4e0d4e18ff4e2d4e37ff4e4a4e51ff4e674e87ff4e9e4eaaff4ec54ed1ff4ed84ee4ff4eff4f1fff4f394f4cff4f6d4f8dff4fb54fccff4fd34ffaff4ff85026ff50445056ff507f5076ff5083509dff50a750b0ff50cb50e7ffff026c
-  Response: 0000000000 ff c1 84b3ee93 0477 ff 00 0b 691c89f0 ff 691c89f0 ff e1 ff 560b560a ff 560f5611 ff 560e5609ff560b560fff5611561aff56195621ff5624562aff561e5612ff5619561fff5614560dff5611560cff5606560eff560b5616ff5613560bff560555fdff55f155eaff55db55d4ff55d155d4ff55ce55d2ff55b955a9ff55a05591ff558a5581ff556a556cff5567556eff55695563ff555d5549ff55315525ff551e5516ff5512550cff55075504ff550054fbff54f354f3ff54e854e5ff54df54ceff54be54b1ff54a854a6ff54a0549eff5499549fff549a5491ff54895489ff54865489ff54875477ff546c5462ff54545449ff5443543cff54335426ff541d5416ff5418540fff540453f6ff53e853ddff53ce53c6ff53ba53b3ff53a85399ff53805371ff5362535cff5355534aff533e5333ff5322530fff52f952ddff52d752c5ff52b952aaff52985283ff52695258ff524d523dff522c5221ff520c51f9ff51ec51e0ff51c651b1ff51a651a4ff51a451a6ff51b251adff51aa51b8ff51b351b8ff51bd51b7ff51b551c7ff51bd51c2ff51b151b6ff51af51b2ff51b051b8ff51b751afff51a05194ff518f5189ff518d5186ff5185517cff51745165ff5154514aff5134512bff511c5109ff50ed50dcff50d150bcff50a95095ff50775060ff50465036ff50215015ff5010500dff50015000ff4ff54fe7ff4fdc4fc6ff4fae4fa1ff4f984f89ff4f814f85ff4f804f74ff4f6d4f6aff4f684f63ff4f5d4f57ff4f534f4aff4f444f42ff4f3b4f34ff4f314f31ff4f284f20ff4f204f1aff4f1a4f1bff4f1a4f17ff4f0a4efeff4efd4efcff4ef94ef1ff4eee4eefff4eed4ee7ff4ee74eecff4ee84ee9ff4ee44edfff4ece4ecdff4ebf4eb3ff4ea74e9bff4e9d4e93ff4e874e78ff4e694e51ff4e3d4e30ff4e184dffff4de34dc0ff4da44d8aff4d6a4d60ff4d504d37ff4d1d4d03ff4ceb4cddff4cc54cb4ff4ca24c93ff4c7f4c67ff4c654c46ff4c2d4c17ff4bff4bd9ff4bce4bd4ff4bcf4bbbff4bc94bbeff4be54be9ff4be54be6ff4bd54bceff4bc64bb0ff4ba74bb2ff4bb54bbeff4bbd4bc4ff4bbc4bb4ff4baa4b8cff4b654b72ff4b644b66ff4b574b5aff4b4e4b31ff4b164b00ff4afd4ae4ff4ac74ab1ff4a9d4a84ff4a7f4a69ff4a5a4a46ff4a404a37ff4a2d4a2aff4a244a18ff4a0f49fcff49f049f6ff49f649f3ff49f549ebff49dd49c0ff499f4994ff498d4976ff4971496aff4979499cff49a349a6ff49914999ff4998499aff49a349a2ff49bb49d1ff49e249f1ff49f549e8ff49fc4a03ff4a0b4a23ff4a354a59ff4a624a7cff4a894aa0ff4abb4ad8ff4ae74aecff4b024b1fff4b504b70ff4b994babff4bb24bb0ff4bd24bdfff4be94c0eff4c264c29ff4c3d4c62ff4c6e4c7bff4c904cb9ff4ce24cf7ff4d084d13ff4d184d06ff4d1e4d36ff4d3c4d3dff4d4d4d68ff4d914db0ff4dcd4ddfff4e0d4e18ff4e2d4e37ff4e4a4e51ff4e674e87ff4e9e4eaaff4ec54ed1ff4ed84ee4ff4eff4f1fff4f394f4cff4f6d4f8dff4fb54fccff4fd34ffaff4ff85026ff50445056ff507f5076ff5083509dff50a750b0ff50cb50e7ffff026c
-  Response: 0000000000 ff c1 84b3ee93 0477 ff 00 0b 691c89f0 ff 691c89f0 ff e1 ff 560b560a ff 560f5611 ff 560e5609ff560b560fff5611561aff56195621ff5624562aff561e5612ff5619561fff5614560dff5611560cff5606560eff560b5616ff5613560bff560555fdff55f155eaff55db55d4ff55d155d4ff55ce55d2ff55b955a9ff55a05591ff558a5581ff556a556cff5567556eff55695563ff555d5549ff55315525ff551e5516ff5512550cff55075504ff550054fbff54f354f3ff54e854e5ff54df54ceff54be54b1ff54a854a6ff54a0549eff5499549fff549a5491ff54895489ff54865489ff54875477ff546c5462ff54545449ff5443543cff54335426ff541d5416ff5418540fff540453f6ff53e853ddff53ce53c6ff53ba53b3ff53a85399ff53805371ff5362535cff5355534aff533e5333ff5322530fff52f952ddff52d752c5ff52b952aaff52985283ff52695258ff524d523dff522c5221ff520c51f9ff51ec51e0ff51c651b1ff51a651a4ff51a451a6ff51b251adff51aa51b8ff51b351b8ff51bd51b7ff51b551c7ff51bd51c2ff51b151b6ff51af51b2ff51b051b8ff51b751afff51a05194ff518f5189ff518d5186ff5185517cff51745165ff5154514aff5134512bff511c5109ff50ed50dcff50d150bcff50a95095ff50775060ff50465036ff50215015ff5010500dff50015000ff4ff54fe7ff4fdc4fc6ff4fae4fa1ff4f984f89ff4f814f85ff4f804f74ff4f6d4f6aff4f684f63ff4f5d4f57ff4f534f4aff4f444f42ff4f3b4f34ff4f314f31ff4f284f20ff4f204f1aff4f1a4f1bff4f1a4f17ff4f0a4efeff4efd4efcff4ef94ef1ff4eee4eefff4eed4ee7ff4ee74eecff4ee84ee9ff4ee44edfff4ece4ecdff4ebf4eb3ff4ea74e9bff4e9d4e93ff4e874e78ff4e694e51ff4e3d4e30ff4e184dffff4de34dc0ff4da44d8aff4d6a4d60ff4d504d37ff4d1d4d03ff4ceb4cddff4cc54cb4ff4ca24c93ff4c7f4c67ff4c654c46ff4c2d4c17ff4bff4bd9ff4bce4bd4ff4bcf4bbbff4bc94bbeff4be54be9ff4be54be6ff4bd54bceff4bc64bb0ff4ba74bb2ff4bb54bbeff4bbd4bc4ff4bbc4bb4ff4baa4b8cff4b654b72ff4b644b66ff4b574b5aff4b4e4b31ff4b164b00ff4afd4ae4ff4ac74ab1ff4a9d4a84ff4a7f4a69ff4a5a4a46ff4a404a37ff4a2d4a2aff4a244a18ff4a0f49fcff49f049f6ff49f649f3ff49f549ebff49dd49c0ff499f4994ff498d4976ff4971496aff4979499cff49a349a6ff49914999ff4998499aff49a349a2ff49bb49d1ff49e249f1ff49f549e8ff49fc4a03ff4a0b4a23ff4a354a59ff4a624a7cff4a894aa0ff4abb4ad8ff4ae74aecff4b024b1fff4b504b70ff4b994babff4bb24bb0ff4bd24bdfff4be94c0eff4c264c29ff4c3d4c62ff4c6e4c7bff4c904cb9ff4ce24cf7ff4d084d13ff4d184d06ff4d1e4d36ff4d3c4d3dff4d4d4d68ff4d914db0ff4dcd4ddfff4e0d4e18ff4e2d4e37ff4e4a4e51ff4e674e87ff4e9e4eaaff4ec54ed1ff4ed84ee4ff4eff4f1fff4f394f4cff4f6d4f8dff4fb54fccff4fd34ffaff4ff85026ff50445056ff507f5076ff5083509dff50a750b0ff50cb50e7ffff026c
+  Received:
+    0000000000 ff c1 84b3ee93 0477
+    ff 00 0b 691c89f0 ff 691c89f0 <- these are timestamps
+    ff e1
+    ff 560b560a ff 560f5611 ff 560e5609 ff 560b560f ff 5611561a
+    ff 56195621 ff 5624562a ff 561e5612 ff 5619561f ff 5614560d
+    ff 5611560c ff 5606560e ff 560b5616 ff 5613560b ff 560555fd
+    ff 55f155ea ff 55db55d4 ff 55d155d4 ff 55ce55d2 ff 55b955a9
+    ff 55a05591 ff 558a5581 ff 556a556c ff 5567556e ff 55695563
+    ff 555d5549 ff 55315525 ff 551e5516 ff 5512550c ff 55075504
+    ff 550054fb ff 54f354f3 ff 54e854e5 ff 54df54ce ff 54be54b1
+    ff 54a854a6 ff 54a0549e ff 5499549f ff 549a5491 ff 54895489
+    ff 54865489 ff 54875477 ff 546c5462 ff 54545449 ff 5443543c
+    ff 54335426 ff 541d5416 ff 5418540f ff 540453f6 ff 53e853dd
+    ff 53ce53c6 ff 53ba53b3 ff 53a85399 ff 53805371 ff 5362535c
+    ff 5355534a ff 533e5333 ff 5322530f ff 52f952dd ff 52d752c5
+    ff 52b952aa ff 52985283 ff 52695258 ff 524d523d ff 522c5221
+    ff 520c51f9 ff 51ec51e0 ff 51c651b1 ff 51a651a4 ff 51a451a6
+    ff 51b251ad ff 51aa51b8 ff 51b351b8 ff 51bd51b7 ff 51b551c7
+    ff 51bd51c2 ff 51b151b6 ff 51af51b2 ff 51b051b8 ff 51b751af
+    ff 51a05194 ff 518f5189 ff 518d5186 ff 5185517c ff 51745165
+    ff 5154514a ff 5134512b ff 511c5109 ff 50ed50dc ff 50d150bc
+    ff 50a95095 ff 50775060 ff 50465036 ff 50215015 ff 5010500d
+    ff 50015000 ff 4ff54fe7 ff 4fdc4fc6 ff 4fae4fa1 ff 4f984f89
+    ff 4f814f85 ff 4f804f74 ff 4f6d4f6a ff 4f684f63 ff 4f5d4f57
+    ff 4f534f4a ff 4f444f42 ff 4f3b4f34 ff 4f314f31 ff 4f284f20
+    ff 4f204f1a ff 4f1a4f1b ff 4f1a4f17 ff 4f0a4efe ff 4efd4efc
+    ff 4ef94ef1 ff 4eee4eef ff 4eed4ee7 ff 4ee74eec ff 4ee84ee9
+    ff 4ee44edf ff 4ece4ecd ff 4ebf4eb3 ff 4ea74e9b ff 4e9d4e93
+    ff 4e874e78 ff 4e694e51 ff 4e3d4e30 ff 4e184dff ff 4de34dc0
+    ff 4da44d8a ff 4d6a4d60 ff 4d504d37 ff 4d1d4d03 ff 4ceb4cdd
+    ff 4cc54cb4 ff 4ca24c93 ff 4c7f4c67 ff 4c654c46 ff 4c2d4c17
+    ff 4bff4bd9 ff 4bce4bd4 ff 4bcf4bbb ff 4bc94bbe ff 4be54be9
+    ff 4be54be6 ff 4bd54bce ff 4bc64bb0 ff 4ba74bb2 ff 4bb54bbe
+    ff 4bbd4bc4 ff 4bbc4bb4 ff 4baa4b8c ff 4b654b72 ff 4b644b66
+    ff 4b574b5a ff 4b4e4b31 ff 4b164b00 ff 4afd4ae4 ff 4ac74ab1
+    ff 4a9d4a84 ff 4a7f4a69 ff 4a5a4a46 ff 4a404a37 ff 4a2d4a2a
+    ff 4a244a18 ff 4a0f49fc ff 49f049f6 ff 49f649f3 ff 49f549eb
+    ff 49dd49c0 ff 499f4994 ff 498d4976 ff 4971496a ff 4979499c
+    ff 49a349a6 ff 49914999 ff 4998499a ff 49a349a2 ff 49bb49d1
+    ff 49e249f1 ff 49f549e8 ff 49fc4a03 ff 4a0b4a23 ff 4a354a59
+    ff 4a624a7c ff 4a894aa0 ff 4abb4ad8 ff 4ae74aec ff 4b024b1f
+    ff 4b504b70 ff 4b994bab ff 4bb24bb0 ff 4bd24bdf ff 4be94c0e
+    ff 4c264c29 ff 4c3d4c62 ff 4c6e4c7b ff 4c904cb9 ff 4ce24cf7
+    ff 4d084d13 ff 4d184d06 ff 4d1e4d36 ff 4d3c4d3d ff 4d4d4d68
+    ff 4d914db0 ff 4dcd4ddf ff 4e0d4e18 ff 4e2d4e37 ff 4e4a4e51
+    ff 4e674e87 ff 4e9e4eaa ff 4ec54ed1 ff 4ed84ee4 ff 4eff4f1f
+    ff 4f394f4c ff 4f6d4f8d ff 4fb54fcc ff 4fd34ffa ff 4ff85026
+    ff 50445056 ff 507f5076 ff 5083509d ff 50a750b0 ff 50cb50e7
+    ff ff 026c
   """
 
 
